b50d71dda87d7aef5c1916928808df5f18c8e01f
Darren <darren@243hotmail.com>
2021-12-14 11:21:59 +0800

    Task 3 incomplete

--- assessment1/src/main/java/myserver/HttpServer.java
+++ assessment1/src/main/java/myserver/HttpServer.java
@@ -1,5 +1,19 @@
 package myserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
-    
+
+    private int port = 3000;
+    public void startServer(String directory, int port) {
+
+        System.out.println(directory);
+        System.out.println(port);
+    }
+
 }
--- assessment1/src/main/java/myserver/Main.java
+++ assessment1/src/main/java/myserver/Main.java
@@ -1,8 +1,33 @@
 package myserver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        int port = 3000;
+        String inputSetting ="";
+        String directory = "static";
+
+        HttpServer server = new HttpServer();
+
+        if (args.length != 0) {
+            for (int i =0; i < args.length; i++) {
+               inputSetting += args[i];
+               inputSetting += ",";
+            }
+            String[] command = inputSetting.trim().split(",");
+            for (int i = 0; i < command.length; i++) {
+                if (command[i].equals("--port") && (i + 1) < command.length) {
+                    port = Integer.parseInt(command[i+1]);
+                } else if (command[i].equals("--docRoot") && (i + 1) < command.length) {
+                    directory = command[i+1];
+                }
+            }
+        }
+        System.out.println("Port set to " + port);
+        System.out.println("Directory set to " + directory);
     }
-    
+        //server.startServer(args[0], args[1]);  
 }
